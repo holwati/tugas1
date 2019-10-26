23618cb4a2c0fc95607912c50cf4e31626c2e5bf
holwati <56399756+holwati@users.noreply.github.com>
2019-10-26 12:57:35 +0700

    Add files via upload

---
+++ latihan 01.py
@@ -0,0 +1,11 @@
+#ini Variabel & Nia
+
+namalengkap = "holwati"
+namapanggilan = "mboy"
+npm = "311910598"
+tempatlahir = "Bekasi"
+umur = "22"
+telf = "0857 7045 4638"
+alamat = "cikarang_sukatani"
+
+print("Assalamualaikum, \n Let me introduce my self. My name is",namalengkap,", but you can call me",namapanggilan,", my npm is",npm,", i was born in",tempatlahir,"and i am",umur,"years old. i am very glad if you want to invite my house in",alamat,", so don't forget to call me before wit the number",telf,"\n thank you" )
